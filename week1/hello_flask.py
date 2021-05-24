df024e43c32688684be6cce35c1b98a724149d9b
heiskane <henipe@gmail.com>
2021-05-24 12:15:37 +0300

    added templates

--- week1/hello_flask.py
+++ week1/hello_flask.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "Will be a home page"
+	return render_template("index.html")
 
 @app.route('/hello')
 def hello():
