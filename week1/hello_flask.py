72a385645841f830099e016e65eec3ebc85414aa
heiskane <henipe@gmail.com>
2021-05-24 13:07:32 +0300

    added markdown filter

--- week1/hello_flask.py
+++ week1/hello_flask.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from flaskext.markdown import Markdown # pip3 install Flask-Markdown
 
 app = Flask(__name__)
+Markdown(app)
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+	return render_template("home.html")
 
 @app.route('/hello')
 def hello():
