6bf1e9e1146f9012113de750f51721275361866d
heiskane <henipe@gmail.com>
2021-05-25 18:01:53 +0300

    added another page to temps

--- day2/employee_list_app/employee_app.py
+++ day2/employee_list_app/employee_app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4asdasdasdQ8z\n\xec]/'
 db = SQLAlchemy(app)
 
 class Employee(db.Model):
@@ -20,11 +21,17 @@ def home():
 @app.route('/add_employee', methods=['POST'])
 def add_employee():
 	employee = Employee()
-	employee.first_name = request.form['first_name']
-	employee.last_name = request.form['last_name']
-	employee.email = request.form['email']
-	db.session.add(employee)
-	db.session.commit()
+	first_name = request.form['first_name']
+	last_name = request.form['last_name']
+	email = request.form['email']
+	if first_name and last_name and email:
+		employee.first_name = first_name
+		employee.last_name = last_name
+		employee.email = email
+		db.session.add(employee)
+		db.session.commit()
+	else:
+		flash("Missing required fields")
 	return redirect(url_for('home'))
 
 @app.route('/delete_employee', methods=['POST'])
@@ -48,6 +55,8 @@ def update_employee():
 		employee.last_name = last_name
 		employee.email = email
 		db.session.commit()
+	else:
+		flash("Missing required fields")
 	return redirect(url_for('home'))
 
 if __name__ == '__main__':
--- day2/temps/templating.py
+++ day2/temps/templating.py
@@ -13,5 +13,9 @@ def home():
 	names = ["bob", "joe", "alice", "traunt"]
 	return render_template("home.html", title="Hello Variable", names=names)
 
+@app.route("/other")
+def other():
+	return render_template('other.html')
+
 if __name__ == "__main__":
 	app.run(debug=True)
