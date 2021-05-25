18cfbceeb54d3684d4a684e37ba198082af10604
heiskane <henipe@gmail.com>
2021-05-25 09:33:55 +0300

    added layout template

--- day2/temps/templating.py
+++ day2/temps/templating.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return render_template("layout.html")
+	return render_template("home.html", title="Hello Variable")
 
 if __name__ == "__main__":
 	app.run(debug=True)
