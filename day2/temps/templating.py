a973ae61f531396aa4e62ddb11c25f1ff1e2c582
heiskane <henipe@gmail.com>
2021-05-25 10:15:50 +0300

    added for loop

--- day2/temps/templating.py
+++ day2/temps/templating.py
@@ -1,10 +1,15 @@
+import sys
+# Only run on python3
+assert sys.version_info.major == 3
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return render_template("home.html", title="Hello Variable")
+	names = ["bob", "joe", "alice", "traunt"]
+	return render_template("home.html", title="Hello Variable", names=names)
 
 if __name__ == "__main__":
 	app.run(debug=True)
