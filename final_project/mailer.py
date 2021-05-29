114f6f2c65f85e5f63042cd7b68a681b4c3770ed
heiskane <henipe@gmail.com>
2021-05-29 12:06:59 +0300

    adder mailer script to final project

---
+++ final_project/mailer.py
@@ -0,0 +1,42 @@
+from requests import post
+from dotenv import load_dotenv # sudo apt install python3-dotenv
+from os import getenv
+
+# Requires .env file with the content:
+# API_KEY = asdasdasdasdasd
+
+load_dotenv()
+API_KEY = getenv('API_KEY')
+headers = {'Authorization': f'Bearer {API_KEY}'}
+
+def send_mail(recipient, subject, content, sender="bot@mypythonproject.rocks"):
+	"""
+	from mailer import send_mail   
+	send_mail(
+		recipient="henipe@gmail.com",
+		subject="subject",
+		content="Hello Potato") 
+	"""
+	#URL = 'https://api.sendgrid.com/v3/mail/send'
+	URL = 'http://127.0.0.1'
+	json = {
+		"personalizations": [
+			{ "to": [
+					{
+						"email": f"{recipient}"
+					}]}],
+		"from": {
+			"email": f"{sender}"
+		},
+		"subject": f"{subject}",
+		"content": [
+			{
+				"type": "text/plain",
+				"value": f"{content}"}]}
+	post(URL, json=json, headers=headers)
+
+if __name__ == '__main__':
+	send_mail(
+		recipient="henipe@gmail.com",
+		subject="Potato",
+		content="Hello World!")
